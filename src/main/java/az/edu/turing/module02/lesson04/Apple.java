8304911ba91cdc7996f5d5cfddfb44641aa295cb
Elvin Taghizade <elvintaghiyev184@gmail.com>
2024-02-19 20:52:21 +0400

    module01 lesson04 class code attached

---
+++ src/main/java/az/edu/turing/module02/lesson04/Apple.java
@@ -0,0 +1,51 @@
+package az.edu.turing.module02.lesson04;
+
+public class Apple extends Fruit {
+
+    private String color;
+    private double weight;
+    private String origin;
+
+    public Apple(String name, String color, double weight, String origin) {
+        super(name);
+        this.color = color;
+        this.weight = weight;
+        this.origin = origin;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(toString() + " is eaten!");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Apple{name='%s', color='%s', weight=%s, origin='%s'}",
+                super.getName(), color, weight, origin);
+    }
+
+}
---
+++ src/main/java/az/edu/turing/module02/lesson04/Fruit.java
@@ -0,0 +1,27 @@
+package az.edu.turing.module02.lesson04;
+
+public class Fruit {
+
+    private String name;
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void eat() {
+        System.out.println(this.name + " is eaten!");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Fruit{name='%s'}", name);
+    }
+}
