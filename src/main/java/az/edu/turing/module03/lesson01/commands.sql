ef7ed8fd8712ae85af1292ef408667185b4b5d37
Elvin Taghizade <elvin.taghizade@ext.careem.com>
2024-05-15 12:37:23 +0400

    module03 lesson01 class code and info attached

---
+++ src/main/java/az/edu/turing/module03/lesson01/commands.sql
@@ -0,0 +1,41 @@
+CREATE
+    DATABASE console;
+
+-- DROP DATABASE "booking-ui";
+
+CREATE TABLE people
+(
+    person_id int,
+    lastname  varchar(255),
+    firstname varchar(255),
+    address   varchar(255),
+    city      varchar(255)
+);
+
+SELECT *
+FROM people;
+
+-- DROP TABLE people;
+
+ALTER TABLE people
+    ALTER
+        COLUMN person_id TYPE bigint;
+
+INSERT INTO people
+VALUES (5, 'Taghizade', 'Elvin2', 'Nowhere', 'ISMAYILLI');
+
+INSERT INTO people(lastname, firstname)
+VALUES ('Taghizade2', 'Elvin2');
+
+SELECT DISTINCT(lastname)
+from people;
+
+SELECT *
+FROM people
+WHERE city IS NOT NULL
+  AND person_id = 1;
+
+SELECT *
+FROM people
+ORDER BY firstname DESC
+--                          person_id DESC;
