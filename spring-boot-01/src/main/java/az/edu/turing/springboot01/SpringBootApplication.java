e1a1364b7a3d8f98766f9c8f2915c6c8618196a4
Elvin Taghizade <elvin.taghizade@ext.careem.com>
2024-07-08 06:44:10 +0400

    module04 lesson02 class code attached

---
+++ spring-boot-01/src/main/java/az/edu/turing/springboot01/SpringBootApplication.java
@@ -0,0 +1,11 @@
+package az.edu.turing.springboot01;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+
+}
---
+++ spring-boot-01/src/test/java/az/edu/turing/springboot01/SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.springboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
